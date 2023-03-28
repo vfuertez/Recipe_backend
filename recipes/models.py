976e2f6ecefec92d40db502913858b4010db34db
Vincent Fuertez <vfuertez@gmail.com>
2023-03-28 02:18:32 -0400

    model changed, migrate error

--- recipes/models.py
+++ recipes/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Recipe(models.Model):
-    name = models.CharField(max_length=250, blank=True)
+    name = models.CharField(max_length=250)
     image = models.CharField(max_length=250, blank=True, null=True)
-    ingredients = models.CharField(max_length=250, blank=True, null=True)
-    recipe = models.CharField(max_length=250, blank=True, null=True)
+    ingredients = models.TextField(max_length=1000, blank=True, null=True)
+    recipe = models.TextField(max_length=1000, blank=True, null=True)
     
